a351fe2a237685cd61a0bf108c7a098ebacf7cc5
Shreeraam G <shreeram0312@gmail.com>
2023-11-14 08:49:41 +0530

    Removed class notes

--- src/day3/coffeeShop/Cart.jsx
+++ src/day3/coffeeShop/Cart.jsx
@@ -1,7 +1,9 @@
 const Cart = ({ products }) => {
   return (
     <div className="px-6">
-      <h1 className="text-2xl font-bold">Cart</h1>
+      <button className="flex gap-x-2 py-2 bg-red-500 rounded-lg px-4">
+        <h1 className="font-bold">Cart</h1>
+      </button>
       <ul>
         {products.map((item, index) => (
           <li key={index}>{item.name}</li>
