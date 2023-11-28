22ae92e674c92ae04b0e44da4365fe9af8c798c8
Shreeraam G <shreeram0312@gmail.com>
2023-11-28 09:07:43 +0530

    Effect Hook

---
+++ src/day13/EffectHook.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+
+const ReactApp = () => {
+  const [message, setMessage] = useState("Hello dear one, learning React...?");
+
+  useEffect(() => {
+    console.log("useEffect hook called");
+
+    const timeoutId = setTimeout(() => {
+      setMessage("Great.... This is time to learn about HOOKS");
+    }, 2000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default ReactApp;
---
+++ src/day13/FormData.jsx
@@ -0,0 +1,7 @@
+import { Container } from "@mui/material";
+
+const FormData = () => {
+  return <Container>FormData</Container>;
+};
+
+export default FormData;
